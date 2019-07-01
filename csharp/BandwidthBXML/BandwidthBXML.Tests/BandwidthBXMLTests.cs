332e3ddd0dd4f47e3cf1189b1717bd2bb9945a26
Jacob Mulford <jmulford@bandwidth.com>
2019-07-01 09:19:48 -0400

    Added gather tests

--- csharp/BandwidthBXML/BandwidthBXML.Tests/BandwidthBXMLTests.cs
+++ csharp/BandwidthBXML/BandwidthBXML.Tests/BandwidthBXMLTests.cs
@@ -51,8 +51,6 @@ namespace BandwidthBXML.Tests
             string response_xml = response.ToXml();
             string expected_xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><Response>  <PlayAudio username=\"user\" password=\"pass\">https://test.com</PlayAudio></Response>";
 
-            //Console.WriteLine(response_xml);
-
             Assert.Equal(response_xml,expected_xml);
         }
 
@@ -82,8 +80,6 @@ namespace BandwidthBXML.Tests
             string response_xml = response.ToXml();
             string expected_xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><Response>  <SendDtmf>123</SendDtmf></Response>";
 
-            //Console.WriteLine(response_xml);
-
             Assert.Equal(response_xml,expected_xml);
         }
 
@@ -100,7 +96,90 @@ namespace BandwidthBXML.Tests
             string response_xml = response.ToXml();
             string expected_xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><Response>  <Forward to=\"+18888888888\" from=\"+17777777777\" callTimeout=\"0\" diversionTreatment=\"none\" diversionReason=\"away\" /></Response>";
 
-            Console.WriteLine(response_xml);
+            Assert.Equal(response_xml,expected_xml);
+        }
+
+        [Fact]
+        public void TestTransferResponse()
+        {
+            Forward forward = new Forward();
+            forward.To = "+18888888888";
+            forward.From = "+17777777777";
+            forward.DiversionTreatment = "none";
+            forward.DiversionReason = "away";
+            Response response = new Response(forward);
+
+            string response_xml = response.ToXml();
+            string expected_xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><Response>  <Forward to=\"+18888888888\" from=\"+17777777777\" callTimeout=\"0\" diversionTreatment=\"none\" diversionReason=\"away\" /></Response>";
+
+            Assert.Equal(response_xml,expected_xml);
+        }
+
+        [Fact]
+        public void TestGatherResponseNoNestedVerb()
+        {
+            Gather gather = new Gather();
+            gather.GatherUrl = "https://test.com";
+            gather.GatherMethod = "POST";
+            gather.TerminatingDigits = "123";
+            gather.Tag = "tag";
+            gather.MaxDigits = 3;
+            gather.InterDigitTimeout = 3;
+            gather.Username = "user";
+            gather.Password = "pass";
+            gather.FirstDigitTimeout = 3;
+            Response response = new Response(gather);
+
+            string response_xml = response.ToXml();
+            string expected_xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><Response>  <Gather gatherUrl=\"https://test.com\" gatherMethod=\"POST\" terminatingDigits=\"123\" tag=\"tag\" maxDigits=\"3\" interDigitTimeout=\"3\" username=\"user\" password=\"pass\" firstDigitTimeout=\"3\" /></Response>";
+
+            Assert.Equal(response_xml,expected_xml);
+        }
+
+        [Fact]
+        public void TestGatherResponseNestedSpeakSentence()
+        {
+            Gather gather = new Gather();
+            gather.GatherUrl = "https://test.com";
+            gather.GatherMethod = "POST";
+            gather.TerminatingDigits = "123";
+            gather.Tag = "tag";
+            gather.MaxDigits = 3;
+            gather.InterDigitTimeout = 3;
+            gather.Username = "user";
+            gather.Password = "pass";
+            gather.FirstDigitTimeout = 3;
+            SpeakSentence speakSentence = new SpeakSentence();
+            speakSentence.Sentence = "Test";
+            gather.SpeakSentence = speakSentence;
+            Response response = new Response(gather);
+
+            string response_xml = response.ToXml();
+            string expected_xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><Response>  <Gather gatherUrl=\"https://test.com\" gatherMethod=\"POST\" terminatingDigits=\"123\" tag=\"tag\" maxDigits=\"3\" interDigitTimeout=\"3\" username=\"user\" password=\"pass\" firstDigitTimeout=\"3\">    <SpeakSentence>Test</SpeakSentence>  </Gather></Response>";
+
+            Assert.Equal(response_xml,expected_xml);
+        }
+
+        [Fact]
+        public void TestGatherResponseNestedPlayAudio()
+        {
+            Gather gather = new Gather();
+            gather.GatherUrl = "https://test.com";
+            gather.GatherMethod = "POST";
+            gather.TerminatingDigits = "123";
+            gather.Tag = "tag";
+            gather.MaxDigits = 3;
+            gather.InterDigitTimeout = 3;
+            gather.Username = "user";
+            gather.Password = "pass";
+            gather.FirstDigitTimeout = 3;
+            PlayAudio playAudio = new PlayAudio();
+            playAudio.Url = "https://test.com";
+            gather.PlayAudio = playAudio;
+            Response response = new Response(gather);
+
+            string response_xml = response.ToXml();
+            string expected_xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><Response>  <Gather gatherUrl=\"https://test.com\" gatherMethod=\"POST\" terminatingDigits=\"123\" tag=\"tag\" maxDigits=\"3\" interDigitTimeout=\"3\" username=\"user\" password=\"pass\" firstDigitTimeout=\"3\">    <PlayAudio>https://test.com</PlayAudio>  </Gather></Response>";
 
             Assert.Equal(response_xml,expected_xml);
         }
