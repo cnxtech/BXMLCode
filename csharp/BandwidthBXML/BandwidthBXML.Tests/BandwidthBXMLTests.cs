3b34d769ae5d7ac5b07f0eac99ef2be5e33f3325
jmulford-bandwidth <jmulford@bandwidth.com>
2019-07-19 12:00:56 -0400

    Added chaining verb example and test to C#

--- csharp/BandwidthBXML/BandwidthBXML.Tests/BandwidthBXMLTests.cs
+++ csharp/BandwidthBXML/BandwidthBXML.Tests/BandwidthBXMLTests.cs
@@ -351,5 +351,33 @@ namespace BandwidthBXML.Tests
 
             Assert.Equal(response_xml,expected_xml);
         }
+
+        /**
+         * Test for SpeakSentence and PlayAudio
+         */
+        [Fact]
+        public void TestSpeakSentencePlayAudioResponse()
+        {
+            SpeakSentence speakSentence = new SpeakSentence();
+            speakSentence.Sentence = "test";
+            speakSentence.Locale = "en_US";
+            speakSentence.Voice = "susan";
+            speakSentence.Gender = "female";
+
+            PlayAudio playAudio = new PlayAudio();
+            playAudio.Url = "https://test.com";
+            playAudio.Username = "user";
+            playAudio.Password = "pass";
+
+            Response response = new Response();
+            response.Add(speakSentence);
+            response.Add(playAudio);
+
+
+            string response_xml = response.ToXml();
+            string expected_xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><Response>  <PlayAudio username=\"user\" password=\"pass\">https://test.com</PlayAudio></Response>";
+
+            Assert.Equal(response_xml,expected_xml);
+        }
     }
 }
